54d820f435580037f1b2b98cd3c2072a41682b02
Bushra Aboubida <bushra.ebox@gmail.com>
2025-04-25 11:15:38 -0700

    CRUD

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const taskRoutes = require('./routes/taskRoutes');
-require('dotenv').config();
-
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const pool = require("../config/db");
+const taskRoutes = require("./routes/taskRoutes");
+require("dotenv").config();
 
-app.use(cors());
 app.use(express.json());
 
-app.use('/api/tasks', taskRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Library Management System is live");
 });
+
+// Test DB route
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("DB error");
+  }
+});
+
+app.use("/api/tasks", taskRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const taskController = require('../controllers/taskController');
 
-// Dummy test route
-router.get('/', (req, res) => {
-  res.json({ message: 'Tasks route working!' });
-});
+router.get('/', taskController.getTasks);
+router.post('/', taskController.createTask);
+router.put('/:id/complete', taskController.markTaskAsComplete);
+router.delete('/:id', taskController.deleteTask);
 
 module.exports = router;
